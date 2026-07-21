44cb55604d3d9fec6e27aa456e875925585d8a2f
William Moss <william@kingjez.com>
2026-07-21 19:50:28 +0100

    William has added a load of cards.

---
+++ python-games/hello/variables.py
@@ -0,0 +1,67 @@
+card1 = "1 hearts"
+card2 = "2 hearts"
+card3 = "3 hearts"
+card4 = "4 hearts"
+card5 = "5 hearts"
+card6 = "6 hearts"
+card7 = "7 hearts"
+card8 = "8 hearts"
+card9 = "9 hearts"
+card10 = "10 hearts"
+card11 = "Jack hearts"
+card12 = "Queen hearts"
+card13 = "King hearts"
+
+card14 = "1 diamonds"
+card15 = "2 diamonds"
+card16 = "3 diamonds"
+card17 = "4 diamonds"
+card18 = "5 diamonds"
+card19 = "6 diamonds"
+card20 = "7 diamonds"
+card21 = "8 diamonds"
+card22 = "9 diamonds"
+card23 = "10 diamonds"
+card24 = "Jack diamonds"
+card25 = "Queen diamonds"
+card26 = "King diamonds"
+
+card27 = "1 clubs"
+card28 = "2 clubs"
+card29 = "3 clubs"
+card30 = "4 clubs"
+card31 = "5 clubs"
+card32 = "6 clubs"
+card33 = "7 clubs"
+card34 = "8 clubs"
+card35 = "9 clubs"
+card36 = "10 clubs"
+card37 = "Jack clubs"
+card38 = "Queen clubs"
+card39 = "King clubs"
+
+card40 = "1 spades"
+card41 = "2 spades"
+card42 = "3 spades"
+card43 = "4 spades"
+card44 = "5 spades"
+card45 = "6 spades"
+card46 = "7 spades"
+card47 = "8 spades"
+card48 = "9 spades"
+card49 = "10 spades"
+card50 = "Jack spades"
+card51 = "Queen spades"
+card52 = "King spades"
+
+card53 = "joker1"
+card54 = "joker2"
+
+cards = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10,
+         card11, card12, card13, card14, card15, card16, card17, card18, card19,
+         card20, card21, card22, card23, card24, card25, card26, card27, card28,
+         card29,card30, card31, card32, card33, card34, card35, card36,
+         card37, card38, card39, card40, card41, card42, card43, card44, card45,
+         card46, card47, card48, card49, card50, card51, card52, card53, card54]
+
+print(cards[38])
