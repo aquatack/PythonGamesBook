0b66c96a787056aba1643a907b7dfd13a7e2cd97
William Moss <william@kingjez.com>
2026-08-11 09:04:49 +0100

    if else

---
+++ python-games/hello/pg_35.py
@@ -0,0 +1,17 @@
+spells = 11
+if (spells > 10):
+    print("you gained the title Enchanter!")
+
+game_over = True
+if game_over:
+    print("Game Over!")
+else:
+    print("keep playing!")
+
+ghosts = 3
+if ghosts > 1:
+    print("Its so spoooooky!")
+elif ghosts > 0:
+    print("get that ghost!")
+else:
+    print("Nooooo all the ghosts are gone!")
