5c4653f2c3955a23ed6b88e06748ee089c68216a
William Moss <william@kingjez.com>
2026-08-14 08:57:38 +0100

    finished loops

--- python-games/hello/pg_36-39.py
+++ python-games/hello/pg_36-39.py
@@ -4,9 +4,18 @@
 # for robot in robots:
 #     #print("I am a robot. My name is " + robot)
 
-robots = ["R2D2", "C3PO", "WALL-E", "Optimus Prime", "Bender", "HAL 9000"]
-colours = ["red", "blue", "green", "yellow", "purple", "orange"]
-index = 0
-for each_robot in robots:
-    print("I am a robot. My name is " + robots[index] + " i am " + colours[index])
-    index = index + 1
+# robots = ["R2D2", "C3PO", "WALL-E", "Optimus Prime", "Bender", "HAL 9000"]
+# colours = ["red", "blue", "green", "yellow", "purple", "orange"]
+# index = 0
+# for each_robot in robots:
+#     print("I am a robot. My name is " + robots[index] + " i am " + colours[index])
+#     index = index + 1
+
+# answer = input("Throw a water balloon? (y/n)")
+# while answer == "y":
+#     print("splash!")
+#     answer = input("Throw another water balloon? (y/n)")
+# print("Goodbye!")
+
+while True:
+    print("this is an infinity loop")
